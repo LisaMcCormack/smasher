5532e2045a7d5c7914383051b806ca020d58f78f
lisamc <lisamccormack85@gmail.com>
2020-04-09 11:32:51 +0100

    changed testing library as previous giving false positives

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/smasher', (_, res) => {
-    res.send('hello world')
+  res.send('hello world')
 })
 
 const server = app.listen(3000, () => console.log("server started on port 3000"))
--- index.test.js
+++ index.test.js
@@ -1,11 +1,23 @@
-const request = require('supertest')
+var chai = require('chai')
+  , chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
 const server = require('./index')
+
+var expect = chai.expect;
 
 
 describe("Smasher", () => {
   it('returns a 200 status code and hello world', (done) => {
-    request(server).get('/smasher').expect(200, 'hello world')
-    server.close(done)
+    chai.request(server)
+      .get('/smasher')
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(res).to.have.status(200)
+        done()
+      })
   })
 })
 
