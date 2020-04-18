860095cce04dd2848bf1c4f2a13d9ccd2f5312fe
lisamc <lisamccormack85@gmail.com>
2020-04-18 18:27:41 +0100

    take business logic out of express router

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const fetch = require('node-fetch')
 const app = express()
 
 
-app.get('/smasher', (req, res) => {
-  const urls = req.query.urls
-  Promise.all(urls.map(url => fetch(url.toString())))
-    .then(resp => Promise.all(resp.map(r => r.text()))
-      .then(result => {
-        res.write(result.join())
-        res.end()
-      })
-    .catch(err => console.log(err)))
-
+app.get('/smasher', async (req, res) => {
+  const bodies = await smasher(req.query.urls)
+  res.send(bodies)
 })
+
+async function smasher(urls) {
+  let result = ""
+  await Promise.all(urls.map(url => fetch(url.toString())))
+    .then(resp => Promise.all(resp.map(r => r.text()))
+      .then(r => {result = r.join()})
+      .catch(err => console.log(err)))
+  return result
+}
 
 const server = app.listen(3000, () => console.log("server started on port 3000"))
 
